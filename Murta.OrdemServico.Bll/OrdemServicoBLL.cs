28edb3b8133977ad67b81ecc939935c547989f24
Júlio César Souza Murta <souzamurtaj@gmail.com>
2015-03-10 03:31:05 +0000

    - Novos projetos

---
+++ Murta.OrdemServico.Bll/OrdemServicoBLL.cs
@@ -0,0 +1,72 @@
+﻿using Murta.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Murta.OrdemServico.Bll
+{
+    public class OrdemServicoBLL
+    {
+        public List<Murta.Dto.OrdemServico> Listar()
+        {
+            // TODO Listar ordens de serviços
+            throw new NotImplementedException();
+        }
+
+        public List<Murta.Dto.OrdemServico> BuscarPorPeriodo(DateTime de, DateTime ate)
+        {
+            // TODO Buscar por data
+            throw new NotImplementedException();
+        }
+
+        public List<Murta.Dto.OrdemServico> BuscarPorPeriodo(DateTime de, DateTime ate, Funcionario funcionario)
+        {
+            // TODO Buscar por data e funcionario
+            throw new NotImplementedException();
+        }
+
+        public List<Murta.Dto.OrdemServico> BuscarPorPeriodo(DateTime de, DateTime ate, Cliente cliente)
+        {
+            // TODO Buscar por data e cliente
+            throw new NotImplementedException();
+        }
+
+        public List<Murta.Dto.OrdemServico> BuscarPorPeriodo(DateTime de, DateTime ate, Funcionario funcionario, Cliente cliente)
+        {
+            // TODO Buscar por data, cliente e funcionario
+            throw new NotImplementedException();
+        }
+
+        public void Cadastrar(Murta.Dto.OrdemServico ordemServico)
+        {
+            // TODO Insere uma nova ordem de servico
+            throw new NotImplementedException();
+        }
+
+        public void CadastrarLote(List<Murta.Dto.OrdemServico> ordensServico)
+        {
+            // TODO Cadastra um lote de ordens de servico
+            throw new NotImplementedException();
+        }
+
+        public void Editar(Murta.Dto.OrdemServico ordemServico)
+        {
+            // TODO Edita uma ordem de servico
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(Murta.Dto.OrdemServico ordemServico)
+        {
+            // TODO Exclui uma ordem de servico
+            throw new NotImplementedException();
+        }
+
+        public void ExcluirLote(List<Murta.Dto.OrdemServico> ordensServico)
+        {
+            // TODO Exclui um lote de ordens de servico
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Murta.OrdemServico.TDD/Inicializador.cs
@@ -0,0 +1,21 @@
+﻿using Murta.Dto;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace Murta
+{
+    public class Inicializador
+    {
+        public static void InicializaConexao()
+        { 
+            string assembly = ConfigurationManager.AppSettings["ActiveDatabaseAssembly"];
+            string connectionClass = ConfigurationManager.AppSettings[ConfigurationManager.AppSettings["ActiveConnectionClass"]];
+            string connectionString = ConfigurationManager.ConnectionStrings[ConfigurationManager.AppSettings["ActiveConnectionString"]].ConnectionString;
+
+            Connection.ConfigureConnection(connectionString: connectionString, assemblyKey: assembly, connectionClass: connectionClass);
+        }
+    }
+}
